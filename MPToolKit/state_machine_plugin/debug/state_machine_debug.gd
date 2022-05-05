08c5185b277346b71a70a2c4a8361724bcf8f55a
pasternatskyi-nikita <pasternatskyi.nikita@gmail.com>
2022-05-05 14:36:13 +0200

    Added debug (now scripts)

---
+++ MPToolKit/state_machine_plugin/debug/state_machine_debug.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+export var pathToStateMachine : NodePath
+
+const BASE_NAME = "State: "
+const METHOD_NAME = "StateChanged"
+onready var label = $RichTextLabel
+
+func _ready():
+	get_node(pathToStateMachine).connect(METHOD_NAME, self, "_on_StateMachine_StateChanged")
+	pass
+
+
+func _on_StateMachine_StateChanged(new_state):
+	var node = new_state as Node
+	label.text = BASE_NAME + node.name
+	pass
